288b85c602f71b4a62e052aa8e4be75b4ac5e180
dmetree01 <nevdk1@gmail.com>
2024-07-09 20:53:50 +0300

    add README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Balance API
+
+Test app for user balance manipulation.
+
+## Installation
+
+Use the npm package manager [npm](https://www.npmjs.com/) to install packages.
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+# copy and fill environment variables
+cp .env.sample .env
+
+# start app
+npm start
+
+# run tests
+npm test
+```
--- index.js
+++ index.js
@@ -7,17 +7,3 @@ const migrations = require('./migrations');
     await migrations(deps);
     await api(deps);
 })();
-
-// (async () => {
-//     const sequelize = new Sequelize('postgres://dmetreereeves@localhost:5432/test1') // Example for postgres
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//         const [results, metadata] = await sequelize.query('SELECT NOW()');
-//         console.log(results, metadata);
-//         sequelize.close();
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//         sequelize.close()
-//     }
-// })();
